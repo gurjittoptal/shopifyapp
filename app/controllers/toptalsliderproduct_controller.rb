209414b6d3e89599fd91e48f2f9847466b8a0b22
hs <gssvanc@toptal-shopify.c.pivotal-rhino-161823.internal>
2017-08-24 23:56:48 +0000

    api changes

--- app/controllers/toptalsliderproduct_controller.rb
+++ app/controllers/toptalsliderproduct_controller.rb
@@ -16,7 +16,20 @@ class ToptalsliderproductController < ShopifyApp::AuthenticatedController
       if aproduct
         result['status'] = 'ok'
 	result['product'] = aproduct
-        result['message'] = ShopifyAPI::Shop.current().id
+        curshopid = ShopifyAPI::Shop.current().id
+        #productData = {'id':aproduct['id'],'title':aproduct['title']}
+        asliderproduct = {}
+	if aproduct.images.length
+          asliderproduct['title'] = aproduct.title
+          asliderproduct['id'] = aproduct.id
+          asliderproduct['src'] = aproduct.images[0].src
+          asliderproduct['width'] = aproduct.images[0].width
+          asliderproduct['height'] = aproduct.images[0].height
+        end
+        
+	createdSlideshopProd = SlideshopProduct.create(shopify_product_id: params[:id], shopid: curshopid, data:JSON.generate(asliderproduct) )         
+        result['message'] = 'Product has been successfully added'
+        result['product'] = asliderproduct 
       else
         result['status'] = 'error'
         result['message'] = 'Product id does not exist'
