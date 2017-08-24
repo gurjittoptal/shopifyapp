8e2d316450b9ff0031944a5b8a11f1f0eec7539d
hs <gssvanc@toptal-shopify.c.pivotal-rhino-161823.internal>
2017-08-24 19:14:49 +0000

    controller and routes added

---
+++ app/controllers/toptalsliderproduct_controller.rb
@@ -0,0 +1,19 @@
+class ToptalsliderproductController < ShopifyApp::AuthenticatedController
+
+  def list
+    processed = []
+    render json: processed
+  end
+
+  def post
+    processed = []
+
+    render json: processed
+  end
+
+  def delete
+    processed = []
+    render json: processed
+  end
+
+end
--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,8 @@ Rails.application.routes.draw do
   root :to => 'home#index'
   mount ShopifyApp::Engine, at: '/'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  delete '/toptalsliderproduct/' => 'toptalsliderproduct#delete'
+  post '/toptalsliderproduct/' => 'toptalsliderproduct#post'
+  get '/toptalsliderproducts/' => 'toptalsliderproduct#list'
 end
