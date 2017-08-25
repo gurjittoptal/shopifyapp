f67bf809efc6726d92ec6b4c8e2bcc94fbf910b8
hs <gssvanc@shoptut.c.pivotal-rhino-161823.internal>
2017-08-25 21:06:47 +0000

    changes

--- app/controllers/toptalsliderproduct_controller.rb
+++ app/controllers/toptalsliderproduct_controller.rb
@@ -1,4 +1,19 @@
 class ToptalsliderproductController < ShopifyApp::AuthenticatedController
+  skip_around_action :shopify_session, only: [:list]
+  skip_before_action :login_again_if_different_shop, only: [:list]
+
+  def list
+    processed = []
+    allproducts = SlideshopProduct.all
+    processed.push({'domain':params[:shop]})
+    allproducts.each do |p|
+      pmetadata = JSON.parse(p.data)
+      processed.push(pmetadata)
+    end
+    log.debug "Prodcut list for toptalslider"
+    render json: processed
+    #render layout: false
+  end
 
   def post
     result = {}
--- config/routes.rb
+++ config/routes.rb
@@ -6,5 +6,5 @@ Rails.application.routes.draw do
   delete '/admin/toptalsliderproduct/' => 'toptalsliderproduct#delete'
   post '/admin/toptalsliderproduct/' => 'toptalsliderproduct#post'
 
-  get '/toptalsliderproducts/' => 'publictoptalsliderproduct#list'
+  get '/toptalsliderproducts/' => 'toptalsliderproduct#list'
 end
