ad0b875f46ec0ed333f32e79507f8b86e71e57a4
hs <gssvanc@instance-1.c.pivotal-rhino-161823.internal>
2017-08-26 16:40:38 +0000

    slider script changes

--- public/toptalslider.js
+++ public/toptalslider.js
@@ -1,1 +1,40 @@
-alert('loaded');
+  (function(){
+
+    var loadScript = function(url, callback){
+    var script = document.createElement("script");
+    script.type = "text/javascript";
+
+    // If the browser is Internet Explorer.
+    if (script.readyState){ 
+      script.onreadystatechange = function(){
+        if (script.readyState == "loaded" || script.readyState == "complete"){
+          script.onreadystatechange = null;
+          callback();
+        }
+      };
+    // For any other browser.
+    } else {
+      script.onload = function(){
+        callback();
+      };
+    }
+
+    script.src = url;
+    document.getElementsByTagName("head")[0].appendChild(script);
+   };
+
+  var myAppJavaScript = function($){
+    alert('<p>Your app is using jQuery version '+$.fn.jquery+'</p>');
+   
+   };
+
+  if ((typeof jQuery === 'undefined') || (parseFloat(jQuery.fn.jquery) < 1.7)) {
+    loadScript('//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js', function(){
+      jQuery191 = jQuery.noConflict(true);
+      myAppJavaScript(jQuery191);
+    });
+  } else {
+    myAppJavaScript(jQuery);
+  }
+
+  })();
